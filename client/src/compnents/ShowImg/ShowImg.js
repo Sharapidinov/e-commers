cb16de7d47b5151745dc1185e1bf91a76d7521bf
Sharapidinov <austie2204@gmail.com>
2022-02-26 13:09:27 +0600

    delet comments

--- client/src/compnents/ShowImg/ShowImg.js
+++ client/src/compnents/ShowImg/ShowImg.js
@@ -3,8 +3,8 @@ import React from 'react';
 const ShowImg = ({it, setShow}) => {
     return (
         <div onClick={() => setShow(false)} className="bg-gray-800 z-10 bg-opacity-80 fixed inset-0 flex m-auto items-center justify-center">
-            <div className=" w-1/2 m-auto flex justify-center items-center">
-                <img className="w-3/4 h-3/4" src={it.image} alt=""/>
+            <div className="sm:w-1/2 m-auto flex justify-center items-center">
+                <img className=" sm:w-3/4 " src={it.image} alt=""/>
             </div>
         </div>
     );
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,35 +3,33 @@ module.exports = {
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
+        screens: {
+            'xs': '370px',
+            'sm': '640px',
+            // => @media (min-width: 640px) { ... }
+
+            'md': '768px',
+            // => @media (min-width: 768px) { ... }
+
+            'lg': '1024px',
+            // => @media (min-width: 1024px) { ... }
+
+            'xl': '1280px',
+            // => @media (min-width: 1280px) { ... }
+
+            '2xl': '1536px',
+            // => @media (min-width: 1536px) { ... }
+        },
+        container: {
+            center: true,
+
+        },
         minHeight: {
             '0': '0',
             '1/4': '25%',
             '1/2': '50%',
             '3/4': '75%',
             'full': '90%',
-        },
-        theme: {
-            screens: {
-                "m" : "420px",
-                'sm': '640px',
-                // => @media (min-width: 640px) { ... }
-
-                'md': '768px',
-                // => @media (min-width: 768px) { ... }
-
-                'lg': '1024px',
-                // => @media (min-width: 1024px) { ... }
-
-                'xl': '1280px',
-                // => @media (min-width: 1280px) { ... }
-
-                '2xl': '1536px',
-                // => @media (min-width: 1536px) { ... }
-            },
-            container: {
-                center: true,
-
-            },
         },
         variants: {},
         plugins: [],
